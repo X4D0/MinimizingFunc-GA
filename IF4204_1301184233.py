6a432b93aa0c4e506934d4aaa2a24ffeb9b7cd34
X4D0 <46711970+X4D0@users.noreply.github.com>
2020-10-16 15:52:28 +0700

    Update again

--- IF4204_1301184233.py
+++ IF4204_1301184233.py
@@ -4,31 +4,31 @@ import random
 # Desain Kromosom dan Metode Pendekodean
 
 ## Binary Encoding using 3 bits (3 Gens)
-def createPopulation(jumlahKromosom,jumlahIndividu):
+def createPopulation(jumlahGen,jumlahKromosom):
         pop = []
         i = 0
-        while i < jumlahIndividu:
+        while i < jumlahKromosom:
                 gen = []
                 j = 0
-                while j<jumlahKromosom:
+                while j<jumlahGen:
                         gen.append(random.randint(0,1))
                         j += 1
-                        pop.append(gen)
+                pop.append(gen)
                 i += 1
         return pop
 
 ## Decode Chromosome
-def decodeChromosome(rminx1, rmax1, rminx2, rmax2,individu):
-        x1 = rminx1 + (rmax1 - rminx1)/(pow(2,-1) + pow(2,-2) + pow(2,-3)) * (individu[0]*pow(2,-1) + individu[1]*pow(2,-2) + individu[2]*pow(2,-3))
-        x2 = rminx2 + (rmax2 - rminx2)/(pow(2,-1) + pow(2,-2) + pow(2,-3)) * (individu[3]*pow(2,-1) + individu[4]*pow(2,-2) + individu[5]*pow(2,-3))
+def decodeChromosome(rminx1, rmax1, rminx2, rmax2,kromosom):
+        x1 = rminx1 + (rmax1 - rminx1)/(pow(2,-1) + pow(2,-2) + pow(2,-3)) * (kromosom[0]*pow(2,-1) + kromosom[1]*pow(2,-2) + kromosom[2]*pow(2,-3))
+        x2 = rminx2 + (rmax2 - rminx2)/(pow(2,-1) + pow(2,-2) + pow(2,-3)) * (kromosom[3]*pow(2,-1) + kromosom[4]*pow(2,-2) + kromosom[5]*pow(2,-3))
         return x1,x2
 
 ## Generate Chromosome Lists
 def decodeChromosomeList(population,rminx1, rmax1, rminx2, rmax2):
         cx1 = []
         cx2 = []
-        for individu in population:
-                x1,x2 = decodeChromosome(rminx1,rmax1,rminx2,rmax2,individu)
+        for kromosom in population:
+                x1,x2 = decodeChromosome(rminx1,rmax1,rminx2,rmax2,kromosom)
                 cx1.append(x1)
                 cx2.append(x2)
         return cx1, cx2
@@ -38,21 +38,13 @@ def fitnessCalc(x1,x2):
         fit = 1/(((np.cos(x1))*(np.sin(x2)))-(x1/(pow(x2,2)+1))+0.0001)
         return fit
 
-def allFitness(jIndividu, cx1, cx2):
+def allFitness(jKromosom, cx1, cx2):
         fitnessAll = []
         i = 0
-        while i < jIndividu:
+        while i < jKromosom:
                 fitnessAll.append(fitnessCalc(cx1[i],cx2[i]))
                 i += 1
         return fitnessAll
-
-def randomFitness(fitness):
-        rnd = []
-        i = 0
-        while i < len(fitness):
-                rnd.append(random.random())
-                i += 1
-        return rnd
 
 # Parent Selection using Roulette Wheel
 def rouletteWheel(fitness, population, random_):
@@ -79,10 +71,9 @@ def crossover(parentsAll,pc):
         childAll = []
         j = 1
         i = 0
-        r = random.random()
         while i < len(parentsAll):
-                if r<pc:
-                        titik = random.randint(1,jKromosom-1)
+                if np.random.uniform(0,1)<pc:
+                        titik = random.randint(1,jGens-1)
                         child1 = parentsAll[i]
                         child2 = parentsAll[j]
                         childAll.append(child1[:titik] + child2[titik:])
@@ -97,7 +88,7 @@ def mutation(childAll,pm):
         i = 0
         while i < len(mutationResult):
                 if random.random() < pm:
-                        rando = random.randint(1,jKromosom-1)
+                        rando = random.randint(1,jGens-1)
                         if mutationResult[i][rando] == 1: mutationResult[i][rando] = 0
                         else: mutationResult[i][rando] = 1
                 i += 1
@@ -105,15 +96,15 @@ def mutation(childAll,pm):
 
 # Steady-State Fitness-Based Procedures
 def steadyState(fitpop,fitmut):
-        new = []
+        baru = []
         i = 0
         while i < len(fitpop):
                 if fitpop[i]<fitmut[i]:
-                        new.append(fitmut[i])
+                        baru.append(fitmut[i])
                 else:
-                        new.append(fitpop[i])
+                        baru.append(fitpop[i])
                 i += 1
-        return new
+        return baru
 
 def finalResult(maxim):
         k = 0
@@ -123,14 +114,15 @@ def finalResult(maxim):
                         hasil = k
                 k += 1
         res = hasil
-        print("Result :\n")
+        print("\nResult :\n")
         print("Best Chromosome\t= ",population[res])
         print("Best Fitness\t= ",fitnessAll[res])
         print("Best x1\t\t= ",x1[res],"\t\tBest x2\t= ", x2[res])
         
 # MAIN
-jKromosom = 6 ## Jumlah Kromosom
-jIndividu = 40 ## Jumlah Individu
+
+jGens = 6 ## Jumlah Gen
+jKromosom = 40 ## Jumlah kromosom
 generations = 115 ## Jumlah Generasi
 ## Probability
 pm = 0.9
@@ -140,10 +132,10 @@ rminx1 = -1
 rmax1 = 2
 rminx2 = -1
 rmax2 = 1
-# Create Population
-populesyen = createPopulation(jKromosom, jIndividu)
-# Decode Chromosome
-x1, x2 = decodeChromosomeList(populesyen,rminx1, rmax1, rminx2, rmax2);
+# Create First Population
+populesyen = createPopulation(jGens, jKromosom)
+# First Decode Chromosome
+x1, x2 = decodeChromosomeList(populesyen,rminx1, rmax1, rminx2, rmax2)
 # Create list for all fitness value
 fitnessAll = []
 
@@ -151,21 +143,17 @@ for i in range(generations): # Stopping Criteria (Max Generations)
         parents = [] # Create Parents List
         fitnessNew = [] # Create list for Final Result of Fitness
         population = populesyen # Assigned Population
-        fitnessAll = allFitness(jIndividu, x1, x2) # Assigned all Fitness value
-        rand = randomFitness(fitnessAll) # Random variable for Roulette Wheel
+        fitnessAll = allFitness(jKromosom, x1, x2) # Assigned all Fitness value
         j = 0
-        while j<jIndividu: # Parent Selection
-                parents.append(rouletteWheel(fitnessAll,population,rand[j]))
+        while j<jKromosom: # Parent Selection
+                parents.append(rouletteWheel(fitnessAll,population,random.random()))
                 j += 1
         childAll = crossover(parents,pc) # Do Crossover
         mutasi = mutation(childAll, pm) # Do Mutation
         x1,x2 = decodeChromosomeList(population,rminx1, rmax1, rminx2, rmax2)
         fitnessNew = allFitness(len(mutasi),x1,x2)
-        # Steady State Fitness-Based
-        population = steadyState(fitnessAll,fitnessNew)
-        print(population)
-        x1,x2 = decodeChromosomeList(population,rminx1, rmax1, rminx2, rmax2)
-        fitnessAll = allFitness(len(population),x1,x2)
+        fitnessAll = steadyState(fitnessAll,fitnessNew)
+        print("Generation ",i+1," DONE!")
 
 maxim = fitnessAll[0]
 finalResult(maxim)
